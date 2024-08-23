e4773c48db97eba2124582428a8d9ef3358ae91e
Your-baiqiang <yiubai@qq.com>
2024-08-24 01:21:09 +0800

    Initial Commit

---
+++ AdmobDemoFile/AdmobDemoFileApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdmobDemoFileApp.swift
+//  AdmobDemoFile
+//
+//  Created by Yiu on 2024/8/24.
+//
+
+import SwiftUI
+
+@main
+struct AdmobDemoFileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
